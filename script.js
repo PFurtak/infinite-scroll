dab5063df9130fc3fcd7cb518dd3a2fb69a4572f
Patrick Furtak <patrick@patrickfurtak.com>
2020-10-24 15:10:59 -0400

    Fetched and rendered data from api

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('Hello World from script.js');
+const postContainer = document.getElementById('post-container');
+const loading = document.querySelector('.loader');
+const filter = document.getElementById('filter');
+
+let limit = 3;
+let page = 1;
+
+async function getPosts() {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  );
+  const data = await res.json();
+  return data;
+}
+
+async function renderPosts() {
+  const posts = await getPosts();
+
+  posts.forEach((post) => {
+    const postElement = document.createElement('div');
+    postElement.classList.add('post');
+    postElement.innerHTML = `
+      <div class="number">${post.id}</div>
+      <div class="post-info">
+        <h2 class="post-title">${post.title}</h2>
+        <p class="post-body">${post.body}</p>
+      </div>
+      `;
+    postContainer.appendChild(postElement);
+  });
+}
+
+renderPosts();
